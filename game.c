27ff8fc8ee861a1bfaee0775aebe85c63d097387
César Nero <161622563+cesarnero@users.noreply.github.com>
2025-10-22 15:58:07 +0100

    Modularization (Contains map and player c and h files)
    Header files included

--- game.c
+++ game.c
@@ -1,57 +1,9 @@
-
 #include <stdio.h>
-#include <stdbool.h>
 
-// Configurations (Static Values)
-#define MAP_ROWS 12
-#define MAP_COLS 12
-
-// Tile codes
-#define TILE_WALL 'W'
-#define TILE_FLOOR 'F'
-#define TILE_POTION 'P'
-
-// Player structure
-struct Player
-{
-    int level;
-    int attack;
-    int defense;
-
-    int position_x;
-    int position_y;
-    // int hp;
-};
-
-// Globals (Map and Player)
-char map[MAP_ROWS][MAP_COLS] = {
-    {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'W', 'W', 'F', 'F', 'P', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'W', 'W', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'W'},
-    {'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'}};
-
-struct Player player = {1, 5, 5, 3, 3};
-
-// Functions to be implemented
-void printMenu(void);
-void printMap(void);
-void printPlayerStats(void);
+#include "map.h"
+#include "player.h"
 
 char readUserInput(void);
-void movePlayer(char dir);
-
-bool isTileWalkable(char t);
-int inBounds(int y, int x);
-
-void collectItems();
 
 // ====== Main ======
 int main(void)
@@ -59,43 +11,19 @@ int main(void)
     printf("Hello Wizard! 🧙‍♂️\n");
     while (1 == 1) {
         // Windows Terminal Clean Function (Uses stdlib)
-        //system("cls");
-        printPlayerStats();
-        printMap();
+        // Don't forget if you use system('cls'), OS like MacOS or Linux will NOT run.
+        // system("cls");
+        player_print_stats();
+        map_print(player_get_row(), player_get_col());
         char input = readUserInput();
-        movePlayer(input);
+        player_move(input);
 
-        collectItems();
+        player_collect_items();
 
         printf("\n\n\n\n\n\n\n");
     }
 
     return 0;
-}
-
-bool isTileWalkable(char t) {
-    return (t != 'W');
-}
-
-void collectItems() {
-    if (map[player.position_x][player.position_y] == 'P') {
-        map[player.position_x][player.position_y] = 'F';
-        player.level++;
-        player.attack += 5;
-        player.defense += 3;
-    }
-    // For example in the future if we have monsters (TILE = 'M')
-    // we can have this:
-    //if (map[player.position_x][player.position_y] == 'M') {
-        // FIGHT HERE!
-    //}
-}
-
-void printPlayerStats() {
-    printf("🗡️ Attack: %d\n", player.attack);
-    printf("🛡️ Defense: %d\n", player.defense);
-    printf("💡 Level: %d\n", player.level);
-    printf("🆇 Player Position: [%d,%d]\n", player.position_x, player.position_y);
 }
 
 char readUserInput(void) {
@@ -105,52 +33,4 @@ char readUserInput(void) {
     char input;
     scanf(" %c", &input);
     return input;
-}
-
-void movePlayer(char dir) {
-    printf("Moving player to %c\n\n", dir);
-    switch(dir) {
-        case 'W':
-        case 'w':
-            if (isTileWalkable(map[player.position_x-1][player.position_y]))
-                player.position_x--;
-            break;
-        case 'A':
-        case 'a':
-            if (isTileWalkable(map[player.position_x][player.position_y-1]))
-                player.position_y--;
-            break;
-        case 'S':
-        case 's':
-            if (isTileWalkable(map[player.position_x+1][player.position_y]))
-                player.position_x++;
-            break;
-        case 'D':
-        case 'd':
-            if (isTileWalkable(map[player.position_x][player.position_y+1]))
-                player.position_y++;
-            break;
-    }
-}
-
-// Implementations
-void printMap(void)
-{
-    for (int x = 0; x < MAP_ROWS; x++)
-    {
-        for (int y = 0; y < MAP_COLS; y++)
-        {
-            if (x == player.position_x && y == player.position_y)
-                printf("🧙");
-            else if (map[x][y] == 'W')
-                printf("🟥");
-            else if (map[x][y] == 'F')
-                printf("⬜️");
-            else if (map[x][y] == 'P')
-                printf("🍔");
-            else
-                printf("%c", map[x][y]);
-        }
-        printf("\n");
-    }
 }
